0ba1cbad07ae514d46970fb13445db5c4554a5ed
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-03-27 19:36:12 +1100

    update 2 - JS done and seems working fine

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,56 @@
-// Current Date and Section
-var currentDay = moment().format("dddd, MMMM Do YYYY");
-$('#currentDay').text(currentDay);
-var currentTime = moment().format("h:mm a");
-$('#currentTime').text(currentTime);
+// List of all Variables
+
+ // Current Date Section
+ var currentDay = moment().format("dddd, MMMM Do YYYY");
+ $('#currentDay').text(currentDay);
+
+ //Save Button
+ var saveButton = $(".saveBtn")
+
+// List of all Functions
+
+ // Each timeblock is color coded to indicate whether it is in the past, present, or future
+ function timeblockColor() {
+     var hour = moment().hours();
+     
+     //
+     $(".time-block").each(function() {
+         var currentHour = parseInt($(".time-block").attr("id"));  
+         
+         if (currentHour < hour) {
+             $(".time-block").addClass("past")
+         } else
+         if (currentHour === hour) {
+             $(".time-block").addClass("present");
+         } else 
+         if (currentHour > hour) {
+             $(".time-block").addClass("future");
+         }
+     })
+ };
+ 
+ // SAVE button on timeblock saves entered event into a local storage
+ saveButton.on("click", function() {
+     var time = $(this).siblings(".hour").text();
+     var information = $(this).siblings(".description").val();
+ 
+     localStorage.setItem(time, information);
+ });
+ 
+ // When refresh the page is refreshed, then the saved events persist
+ function savedEvents() {
+     
+    $(".hour").each(function() {
+        var currentHour = $(this).text();
+        var storedInformation = localStorage.getItem(currentHour);
+        
+        if(storedInformation !== null) {
+            $(this).siblings(".description").val(storedInformation);
+         }
+     });
+ }
+
+// Invoke Functions
+
+timeblockColor();
+savedEvents();
